8357d1858af38dce86512784a30c4ccc965ddd1c
christopher-weber <christopher-weber@web.de>
2020-03-22 22:26:18 +0100

    Database class added
    For each contact person a .db file will be created and named with the hash value of the contact persons public key. Each file contains one line per encounter to an infected person.

--- db/db.h
+++ db/db.h
@@ -10,25 +10,40 @@
 
 class DB{
 public:
-  // Methods
-  DB();
+  DB(std::string _path, std::string _fileEnding = ".db");
   ~DB();
-  std::vector<std::tuple<std::string,std::string,std::string,std::string>>  getEncounters(std::string);
-  void submitEncounters(std::vector<std::tuple<std::string,std::string,std::string,std::string>>);
-  // Members
-  // Signals
-  // Slots
+
+  // getEncounters
+  // Get all encounters of an contact person
+  // Input:
+  //  puplicKeyContactPerson - puplic key of the contact person which wants to know its encounters
+  // Return: vector of all encounters. Each entry containing a tuple of 
+  //   puplicKeyContactPerson	std::string
+  //   puplicKeyInfectedPerson	std::string
+  //   coordinates longitude	double
+  //   coordinate altitude		double
+  //   timestamp [ms]			UINT64
+  std::vector<std::tuple<std::string,std::string,double,double, UINT64>>  getEncounters(std::string puplicKeyContactPerson);
+
+  // getEncounters
+  // Submit all encounters of an infected person
+  // Input:
+  //  vector of all encounters. Each entry containing a tuple of 
+  //   puplicKeyContactPerson	std::string
+  //   puplicKeyInfectedPerson	std::string
+  //   coordinates longitude	double
+  //   coordinate altitude		double
+  //   timestamp [ms]			UINT64
+  void submitEncounters(std::vector<std::tuple<std::string, std::string, double, double, UINT64>>);
+
+
 private:
-  // Methods
-  // Members
+  std::vector<std::string> splitString(std::string _stringToBeSplitted, std::string _delimeter);
+  std::string getFilenameOfContact(std::string _puplicKeyOfContactPerson);
+
+  std::string m_path;
+  std::string m_fileEnding;
   std::mutex mtx;
-  // Signals
-  // Slots
-protected:
-  // Methods
-  // Members
-  // Signals
-  // Slots
 };
 
 #endif // DB_H
